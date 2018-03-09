5616fe8f453f9c53f6a7be1baa20cf2c33784d3e
HolmesConan <holmesconan@gmail.com>
2018-03-09 19:26:19 +0800

    Done the train data generate.

--- tensorflow/ex2/download.py
+++ tensorflow/ex2/download.py
@@ -5,9 +5,9 @@ from codelist import sh50
 
 start_date = '2017-01-01'
 end_date = datetime.today().strftime('%Y-%m-%d')
-filename = 'train_data/stock.hdf5'
+filename = 'train_data/stock_raw.hdf5'
 
 for code in sh50():
 	df = ts.get_k_data(code, start_date, end_date)
-	df.to_hdf(filename, 's' + code)
+	df.to_hdf(filename, 'A' + code)
 	print(code, 'is done')
--- tensorflow/ex2/input_data.py
+++ tensorflow/ex2/input_data.py
@@ -1,21 +1,22 @@
 # -*- coding:utf-8 -*-
-"""Download and draw K graph.
+"""Generate input data.
 """
+import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.finance as mpf
 import datetime
 
 from matplotlib.pylab import date2num
+from codelist import sh50
 
 def date_to_num(dates):
     num_time = []
     for date in dates:
-        date_time = datetime.datetime.strptime(date,'%Y-%m-%d')
+        date_time = date.to_pydatetime()
         num_date = date2num(date_time)
         num_time.append(num_date)
     return num_time
-
 
 def draw(data, filename):
     mat = data.as_matrix()
@@ -23,9 +24,7 @@ def draw(data, filename):
     mat[:, 0] = num_time
 
     fig, ax = plt.subplots(figsize=(15,5))
-    # fig.subplots_adjust(bottom=0.5)
     mpf.candlestick_ochl(ax, mat, width=0.6, colorup='g', colordown='r', alpha=1.0)
-    plt.grid(True)
 
     # 设置日期刻度旋转的角度
     plt.xticks(rotation=30)
@@ -33,7 +32,38 @@ def draw(data, filename):
     ax.xaxis_date ()
 
     fig.savefig(filename)
+    plt.close(fig)
 
-data = download(['002739'], '2017-01-01')
-data[0].to_csv('train_data/002739.csv')
-draw(data[0], 'train_data/k-line.png')
+filename = 'train_data/stock_raw.hdf5'
+
+date_list = []
+
+for code in sh50():
+    df = pd.read_hdf(filename, 'A' + code)
+    mat = df.as_matrix()
+    date_list.extend(mat[:, 0])
+
+date_list = list(set(date_list))
+date_list.sort()
+
+for code in sh50():
+    df = pd.read_hdf(filename, 'A' + code)
+    df['date'] = pd.to_datetime(df['date'])
+    for i in range(len(date_list) - 121):
+        start_date = date_list[i]
+        end_date = date_list[i + 120]
+
+        mask = (df['date'] >= start_date) & (df['date'] < end_date)
+
+        sub_df = df.loc[mask]
+
+        end_df = df.loc[df['date'] == end_date]
+        next_day_df = df.loc[df['date'] == date_list[i + 121]]
+        end_day_close = end_df['close'].iloc[0]
+        next_day_high = next_day_df['high'].iloc[0]
+        label = (end_day_close * 1.01 < next_day_high) and 1 or 0
+
+        kline_filename = 'train_data/%s_%s_%s_%d.png' % (code, start_date, end_date, label)
+        draw(sub_df, kline_filename)
+
+        print (kline_filename, next_day_high, end_day_close)
