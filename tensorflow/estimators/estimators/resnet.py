5f547a6039378cadf7ac7ea147c900ca85165b12
Holmes Conan <holmesconan@gmail.com>
2018-06-07 16:38:19 +0800

    Create tensorflow estimator practice structure.

---
+++ tensorflow/estimators/estimators/resnet.py
@@ -0,0 +1,27 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""ResNet model for classifying images.
+ResNet as proposed in:
+Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+Deep Residual Learning for Image Recognition. arXiv:1512.03385
+CIFAR-10 as in:
+http://www.cs.toronto.edu/~kriz/cifar.html
+"""
+
+def _model_fn():
+    pass
+
+def build_estimator():
+    pass
--- tensorflow/estimators/main.py
+++ tensorflow/estimators/main.py
@@ -1,10 +1,34 @@
-#!/bin/env python3
-# -*- coding: utf-8 -*-
-"""Main script.
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tensorflow estimators for classifying images from CIFAR-10 dataset.
+Support single-host training with one or multiple devices.
 """
 import argparse
+import functools
+import itertools
+import os
 
+#import cifar10
+#import cifar10_model
+#import cifar10_utils
+import numpy as np
 import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
 
 def main(args):
     """Main Function"""
