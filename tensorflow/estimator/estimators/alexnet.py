1395610124abcd4277e0512d3ab81f315b0a7c68
Holmes Conan <holmesconan@gmail.com>
2018-06-12 17:59:18 +0800

    Restructure the project.

--- tensorflow/estimator/estimators/alexnet.py
+++ tensorflow/estimator/estimators/alexnet.py
@@ -8,6 +8,7 @@
 2018-06-10 step=100000 loss = 0.73 accuracy=0.7412
 """
 import tensorflow as tf
+from estimators.utils import build_model_fn
 
 
 def conv2d_layer(inputs, filters, kernel_size, strides=1, **kwargs):
@@ -50,63 +51,11 @@ def construct_model(input_layer, is_training):
     return logits
 
 
-def model_fn(features, labels, mode, params):
-    """Estimator model function.
-
-    Arguments:
-        features {Tensor} -- features in input
-        labels {Tensor} -- labels of features
-        mode {tf.estimator.ModeKeys} -- mode key
-        params {any} -- model params
-    """
-    # Use `input_layer` to apply the feature columns.
-    input_layer = tf.image.resize_images(features['image'], [224, 224])
-
-    logits = construct_model(input_layer, mode == tf.estimator.ModeKeys.TRAIN)
-
-    predictions = {
-        # Generate predictions (for PREDICT and EVAL mode)
-        "classes": tf.argmax(input=logits, axis=1),
-        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-        # `logging_hook`.
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }
-
-    export_outputs = {
-        'predict_output': tf.estimator.export.PredictOutput(predictions)
-    }
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            predictions=predictions,
-            export_outputs=export_outputs)
-
-    # Calculate Loss (for both TRAIN and EVAL modes)
-    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-    
-    accuracy = tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
-    accuracy_mean = tf.reduce_mean(accuracy, name='accuracy_mean')
-    tf.summary.scalar('accuracy_mean', accuracy_mean)
-
-    # Configure the Training Op (for TRAIN mode)
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=params['learning_rate'])
-        train_op = optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-    # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = { "accuracy": accuracy }
-    return tf.estimator.EstimatorSpec(
-        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-
 def build_estimator(config, params):
     """Build an estimator for train and predict.
     """
     return tf.estimator.Estimator(
-        model_fn=model_fn,
+        model_fn=build_model_fn(construct_model, resize_image=True),
         config=config,
         params=params
     )
--- tensorflow/estimator/estimators/resnet.py
+++ tensorflow/estimator/estimators/resnet.py
@@ -7,10 +7,8 @@
 结果记录：
 2018-06-09 step=100000 loss = 1.16 accuracy=0.5826
 """
-import sys
-
-import numpy as np
 import tensorflow as tf
+from estimators.utils import build_model_fn
 
 NUM_LAYERS = 18
 
@@ -52,7 +50,7 @@ def bottleneck_block(inputs, filters, strides=(1,1), with_conv_shortcut=False):
         return tf.add(x, inputs)
 
 
-def construct_model(input_layer, is_training):
+def construct_model(input_layer, is_training, **kwargs):
     """Construct the model."""
     paddings_3 = tf.constant([[0, 0], [3, 3], [3, 3], [0, 0]])
 
@@ -113,63 +111,11 @@ def construct_model(input_layer, is_training):
     return logits
 
 
-def model_fn(features, labels, mode, params):
-    """Estimator model function.
-
-    Arguments:
-        features {Tensor} -- features in input
-        labels {Tensor} -- labels of features
-        mode {tf.estimator.ModeKeys} -- mode key
-        params {any} -- model params
-    """
-    # Use `input_layer` to apply the feature columns.
-    input_layer = tf.image.resize_images(features['image'], [224, 224])
-
-    logits = construct_model(input_layer, mode == tf.estimator.ModeKeys.TRAIN)
-
-    predictions = {
-        # Generate predictions (for PREDICT and EVAL mode)
-        "classes": tf.argmax(input=logits, axis=1),
-        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-        # `logging_hook`.
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }
-
-    export_outputs = {
-        'predict_output': tf.estimator.export.PredictOutput(predictions)
-    }
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            predictions=predictions,
-            export_outputs=export_outputs)
-
-    # Calculate Loss (for both TRAIN and EVAL modes)
-    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-    
-    accuracy = tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
-    accuracy_mean = tf.reduce_mean(accuracy, name='accuracy_mean')
-    tf.summary.scalar('accuracy', accuracy_mean)
-
-    # Configure the Training Op (for TRAIN mode)
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=params['learning_rate'])
-        train_op = optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-    # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = { "accuracy": accuracy }
-    return tf.estimator.EstimatorSpec(
-        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-
 def build_estimator(config, params):
     """Build an estimator for train and predict.
     """
     return tf.estimator.Estimator(
-        model_fn=model_fn,
+        model_fn=build_model_fn(construct_model, resize_image=True),
         config=config,
         params=params
     )
